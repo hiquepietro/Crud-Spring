65c5e63d22a9cf8fe06e2669521ed8c57774513a
Pietro Henrique <pietros@ciandt.com>
2023-07-27 14:53:31 -0300

    Refatoração do controller

--- src/main/java/com/pietro/controller/CursoController.java
+++ src/main/java/com/pietro/controller/CursoController.java
@@ -4,9 +4,14 @@ import com.pietro.model.Curso;
 import com.pietro.repository.CursoRepository;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,9 +24,15 @@ public class CursoController {
     private final CursoRepository cursoRepository;
 
 
-
     @GetMapping
     public @ResponseBody List<Curso> list(){
+
         return cursoRepository.findAll();
     }
+
+   @PostMapping
+   @ResponseStatus(code = HttpStatus.CREATED)
+    public Curso  create(@RequestBody Curso curso) {
+        return cursoRepository.save(curso);
+   }
 }
